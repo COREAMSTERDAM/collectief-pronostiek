fb2ddf179192e7bd9640f6659badcec88f9b2d1c
COREAMSTERDAM <100514235+COREAMSTERDAM@users.noreply.github.com>
2026-06-17 19:52:36 +0200

    fix

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,9 +1,17 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
 
-export const supabase = createClient(
-  supabaseUrl,
-  supabaseAnonKey
-);
+
+const supabaseUrl =
+
+  process.env.NEXT_PUBLIC_SUPABASE_URL || "https://zdrmofnnbgkszpeusclp.supabase.co";
+
+
+
+const supabaseAnonKey =
+
+  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || "sb_publishable_Lc7sogS0VXaOk6tXxtK_zA_3fS8P6qh";
+
+
+
+export const supabase = createClient(supabaseUrl, supabaseAnonKey);
